26929970e74f13a5b01bf81ef570333af796d06d
Odilbek <=>
2026-03-02 12:42:16 +0500

    c

--- apps/common/serializers.py
+++ apps/common/serializers.py
@@ -3,19 +3,15 @@ from .models import UserPermissions
 from django.contrib.contenttypes.models import ContentType
 
 class UserPermissionsSerializer(serializers.ModelSerializer):
-    content_type_name = serializers.SerializerMethodField()
-    
     class Meta:
         model = UserPermissions
         fields = [
-            "id", "content_type", "object_id", "content_type_name",
             "crud_whouse_manager", "read_whouse_manager",
             "crud_factory_operator", "read_factory_operator",
             "crud_driver", "read_driver",
             "crud_guard", "read_guard",
-            "created_at", "updated_at"
+            "crud_product", "read_product",
+            "crud_transport", "read_transport",
+            "crud_client", "read_client",
+            "read_whouse"
         ]
-        read_only_fields = ["id", "created_at", "updated_at"]
-
-    def get_content_type_name(self, obj):
-        return obj.content_type.model
