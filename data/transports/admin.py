b9a49713fb5694d4bf5f563e608a5f009592a927
Odilbek <=>
2026-03-02 14:38:54 +0500

    c

--- data/transports/admin.py
+++ data/transports/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Transport
 
 # Register your models here.
+@admin.register(Transport)
+class TransportAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'type', 'number', 'whouse']
+    list_filter = ['whouse']
+    search_fields = ['name', 'number']
