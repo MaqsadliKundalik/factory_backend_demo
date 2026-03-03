9b6dfa09b8db2374a9d62aed5b2fb388bbde146e
Odilbek <=>
2026-03-03 10:04:36 +0500

    c

--- data/filedatas/admin.py
+++ data/filedatas/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import FileData
 
-# Register your models here.
+@admin.register(FileData)
+class FileDataAdmin(admin.ModelAdmin):
+    list_display = ('id', 'file', "user", 'created_at', 'updated_at')
