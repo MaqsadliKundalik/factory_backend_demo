7006a07d0cef5c980fbd92c6cf4fc3bbc634121e
Odilbek <=>
2026-03-03 09:43:43 +0500

    c

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-359pbm8gkkvm=dv*02#6&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = "*"
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
