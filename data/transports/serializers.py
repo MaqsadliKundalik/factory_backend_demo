e63c4b4f4bea5f093f264a727055dc1ce9ceeabf
Odilbek <=>
2026-03-03 15:49:18 +0500

    c

--- data/transports/serializers.py
+++ data/transports/serializers.py
@@ -6,3 +6,11 @@ class TransportSerializer(serializers.ModelSerializer):
         model = Transport
         fields = ['id', 'name', 'type', 'number', 'whouse']
         read_only_fields = ['id']
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['whouse'] = {
+            'id': instance.whouse.id,
+            'name': instance.whouse.name
+        }
+        return representation
