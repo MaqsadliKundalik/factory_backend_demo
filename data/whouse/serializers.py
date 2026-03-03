0d021b557e88f7334064553a811bafcb1cb60a4e
Odilbek <=>
2026-03-03 10:42:35 +0500

    c

--- data/whouse/serializers.py
+++ data/whouse/serializers.py
@@ -2,9 +2,7 @@ from rest_framework import serializers
 from .models import Whouse
 from data.users.models import FactoryUser
 from apps.drivers.serializers import DriverSerializer
-from apps.whouse_manager.serializers.whouse_manager import WhouseManagerSerializer
-from apps.factory_operator.serializers.factory_operator import FactoryOperatorSerializer
-from apps.guard.serializers import GuardSerializer
+from data.users.serializers import FactoryUserSerializer
 
 class WhouseGetSerializer(serializers.ModelSerializer):
     managers = serializers.SerializerMethodField()
@@ -18,20 +16,19 @@ class WhouseGetSerializer(serializers.ModelSerializer):
 
     def get_managers(self, obj):
         users = FactoryUser.objects.filter(whouses=obj, role='manager')
-        return WhouseManagerSerializer(users, many=True).data
+        return FactoryUserSerializer(users, many=True).data
 
     def get_factory_operators(self, obj):
         users = FactoryUser.objects.filter(whouses=obj, role='operator')
-        return FactoryOperatorSerializer(users, many=True).data
+        return FactoryUserSerializer(users, many=True).data
 
     def get_drivers(self, obj):
-        from apps.drivers.models import Driver
         drivers = Driver.objects.filter(whouse=obj)
         return DriverSerializer(drivers, many=True).data
 
     def get_guards(self, obj):
         users = FactoryUser.objects.filter(whouses=obj, role='guard')
-        return GuardSerializer(users, many=True).data
+        return FactoryUserSerializer(users, many=True).data
 
 class WhouseCreateUpdateSerializer(serializers.ModelSerializer):
     class Meta:
