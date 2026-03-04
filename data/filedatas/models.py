9505409bd96762c9283a0f7139015c74699d93a3
Odilbek <=>
2026-03-04 13:35:55 +0500

    c

--- data/filedatas/models.py
+++ data/filedatas/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.conf import settings
 from apps.common.models import BaseModel
+from data.users.models import FactoryUser
 
 # Create your models here.
 class File(BaseModel):
     file = models.FileField(upload_to='files/')
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(FactoryUser, on_delete=models.CASCADE, null=True, blank=True)
