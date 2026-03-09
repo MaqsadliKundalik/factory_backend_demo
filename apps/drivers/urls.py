e365027592e873b18633c23eec50a066ca1d7357
Odilbek <=>
2026-03-09 13:03:49 +0500

    c

--- apps/drivers/urls.py
+++ apps/drivers/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from apps.drivers.views import DriverListCreateAPIView, DriverRetrieveUpdateDestroyAPIView, DriverPasswordChangeView
 
 urlpatterns = [
-    path("", DriverListCreateAPIView.as_view()),
-    path("<int:pk>/", DriverRetrieveUpdateDestroyAPIView.as_view()),
     path("<int:driver_id>/change-password/", DriverPasswordChangeView.as_view(), name="driver-change-password"),
+    path("<int:pk>/", DriverRetrieveUpdateDestroyAPIView.as_view()),
+    path("", DriverListCreateAPIView.as_view()),
 ]
