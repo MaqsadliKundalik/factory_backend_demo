90198ae4c8eb3fa78afd938dba2e613ba58460c0
Odilbek <=>
2026-03-10 11:43:57 +0500

    c

--- data/clients/admin.py
+++ data/clients/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from .models import Client
+from .models import Client, ClientPhone, ClientBranches
 
 # Register your models here.
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ['name', 'inn_number', 'phone_number', 'whouse']
+    list_display = ['name', 'inn_number', 'whouse']
+
+@admin.register(ClientPhone)
+class ClientPhoneAdmin(admin.ModelAdmin):
+    list_display = ['client', 'phone_number', 'name', 'role']
+
+@admin.register(ClientBranches)
+class ClientBranchesAdmin(admin.ModelAdmin):
+    list_display = ['client', 'name', 'address', 'longitude', 'latitude']
--- data/clients/models.py
+++ data/clients/models.py
@@ -7,6 +7,7 @@ from utils.sayqal import SayqalSms
 
 if TYPE_CHECKING:
     from data.orders.models import Order
+    from data.filedatas.models import File
 
 sayqal = SayqalSms()
 
@@ -14,12 +15,11 @@ sayqal = SayqalSms()
 class Client(BaseModel):
     name = models.CharField(max_length=255)
     inn_number = models.CharField(max_length=9)
-    phone_number = models.CharField(max_length=255)
     photo = models.ForeignKey(File, on_delete=models.SET_NULL, null=True, blank=True)
     whouse = models.ForeignKey('factory_whouse.Whouse', on_delete=models.CASCADE)
-    
-    list_display = ["name", "inn_number", "phone_number", "whouse"]
+    files: "models.QuerySet[File]" = models.ManyToManyField('filedatas.File', related_name='clients', null=True, blank=True)
 
+    list_display = ["name", "inn_number", "whouse", "files"]
 
     orders: "models.QuerySet[Order]" 
 
@@ -40,3 +40,14 @@ class ClientBranches(BaseModel):
     
     def __str__(self):
         return f"{self.name} - {self.client}"
+
+class ClientPhone(BaseModel):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name="phones")
+    phone_number = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    role = models.CharField(max_length=255)
+
+    list_display = ["client", "phone_number", "name", "role"]
+    
+    def __str__(self):
+        return f"{self.phone_number} - {self.name} - {self.role}"
