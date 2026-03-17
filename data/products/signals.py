4ed84a18b9820f7dc1f895df085a3b0ffee61068
Odilbek <=>
2026-03-17 16:43:06 +0500

    d

---
+++ data/products/signals.py
@@ -0,0 +1,37 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import WhouseProducts, WhouseProductsHistory, HistoryStatus
+from data.notifications.models import Notification
+
+@receiver(post_save, sender=WhouseProducts)
+def create_whouse_product_history(sender, instance, **kwargs):
+    if instance.status == WhouseProducts.Status.PENDING:
+        Notification.objects.create(
+            to_role='whouse_manager',
+            from_role='guard',
+            title='New product added',
+            message=f'New product {instance.product.name} added to whouse {instance.whouse.name}',
+        )   
+
+    WhouseProductsHistory.objects.create(
+        wproduct=instance,
+        whouse=instance.whouse,
+        product=instance.product,
+        quantity=instance.quantity,
+        supplier=instance.supplier,
+        status=HistoryStatus.IN
+    )
+
+@receiver(post_save, sender=WhouseProducts)
+def update_whouse_product_history_extra(sender, instance, **kwargs):
+    if instance.status == WhouseProducts.Status.CREATED:
+        WhouseProductsHistory.objects.create(
+            wproduct=instance,
+            whouse=instance.whouse,
+            product=instance.product,
+            quantity=instance.quantity,
+            supplier=instance.supplier,
+            status=HistoryStatus.IN
+        )
+
+
