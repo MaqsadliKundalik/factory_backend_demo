bd46cf460176b58e263af8553cf9139fcb6ef96e
Odilbek <=>
2026-03-17 12:28:23 +0500

    cc

--- data/transports/views.py
+++ data/transports/views.py
@@ -11,6 +11,7 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 TRANSPORT_FILTER_PARAMS = [
     openapi.Parameter(
