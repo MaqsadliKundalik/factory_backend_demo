f9c79271f2bd8d685a1b742b0e040a450cdc0d88
Odilbek <=>
2026-03-23 11:37:11 +0500

    c

--- data/stats/urls.py
+++ data/stats/urls.py
@@ -5,9 +5,7 @@ from .views import (
     SupplierIncomeProductStatsView,
     OutcomingProductStatsView,
     OrderStatusStatsView,
-    OrderStatusDurationStatsView,
     ExcavatorOrderStatusStatsView,
-    ExcavatorStatusDurationStatsView,
 )
 
 urlpatterns = [
@@ -16,7 +14,5 @@ urlpatterns = [
     path('supplier-income-products/', SupplierIncomeProductStatsView.as_view(), name='supplier-income-product-stats'),
     path('outcome-products/', OutcomingProductStatsView.as_view(), name='outcome-product-stats'),
     path('orders/', OrderStatusStatsView.as_view(), name='orders-stats'),
-    path('orders/status/', OrderStatusDurationStatsView.as_view(), name='orders-status-duration-stats'),
     path('excavator/orders/', ExcavatorOrderStatusStatsView.as_view(), name='excavator-orders-stats'),
-    path('excavator/orders/status/', ExcavatorStatusDurationStatsView.as_view(), name='excavator-orders-status-duration-stats'),
 ]
