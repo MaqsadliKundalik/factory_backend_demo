c93c30c37111511e14bc825a0f6992cfbcefb17f
Odilbek <=>
2026-03-30 09:35:39 +0500

    D

--- data/users/urls.py
+++ data/users/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import SimpleRouter
-from .views import FactoryUserResetPasswordViewSet, FactoryUserViewSet
+from .views import FactoryUserResetPasswordViewSet, FactoryUserViewSet, UserSelectView
 
 router = SimpleRouter()
 router.register(
@@ -8,5 +8,6 @@ router.register(
     basename="factory-user-reset-password",
 )
 router.register("", FactoryUserViewSet, basename="factory-user")
+router.register("select", UserSelectView, basename="factory-user-select")
 
 urlpatterns = router.urls
